0436ddc67780c7c945995a21837f6f83239ffb83
ChaboCode <kaerdos.dev@gmail.com>
2023-04-02 09:28:53 -0600

    Integrated Prisma

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -1,5 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
 
-export default function IndexHandler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'Jhon Doe' })
+function IndexHandler(req: NextApiRequest, res: NextApiResponse) {
+  const prisma = new PrismaClient()
+
+  async function queries() {
+    const allData = await prisma.test.findMany()
+    console.log(allData)
+  }
+
+  queries()
+    .then(async () => {
+      await prisma.$disconnect()
+      res.status(200).json({ name: 'Jhon Doe' })
+    })
+    .catch(async e => {
+      console.error(e)
+      res.status(500)
+      await prisma.$disconnect()
+      process.exit(1)
+    })
 }
+
+export default IndexHandler 
