396a2f8e8c963f388961d67eefb54d9dcda99ec1
ChaboCode <kaerdos.dev@protonmail.com>
2023-04-25 11:29:17 -0600

    It's MegamiShop not MonaShop

--- components/MegamiHead.tsx
+++ components/MegamiHead.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function MegamiHead() {
     return (
         <Head>
-            <title>MonaShop</title>
+            <title>MegamiShop</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
