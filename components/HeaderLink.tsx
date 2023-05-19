b0bfd4bd21d292defe4383f1ad0946fb5a1c68e1
ChaboCode <kaerdos.dev@protonmail.com>
2023-05-19 00:04:13 -0600

    Implemented header links

--- components/HeaderLink.tsx
+++ components/HeaderLink.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image'
+import Link from "next/link";
 
 import styles from '@/styles/Home.module.css'
 
 interface HeaderLinkParams {
     svg: any,
-    text: String,
+    text: string,
+    href: string,
 }
 
-export default function HeaderLink({ svg, text }: HeaderLinkParams) {
+export default function HeaderLink({ svg, text, href }: HeaderLinkParams) {
     return (
         <>
-            <a className={styles.headerIconLink}>
+            <Link href={href} className={styles.headerIconLink}>
                 <Image className={styles.headerIcon} src={svg} alt="Box" />
                 <div className={styles.headerLinkText}>{text}</div>
-            </a>
+            </Link>
         </>
     )
 }
