af147aee33ea2fe4ad6f66f98827f69075fb1086
ChaboCode <kaerdos.dev@protonmail.com>
2023-06-21 16:40:05 -0600

    Small host fix

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: process.env.MINIO_ENDPOINT,
-        port: '9000',
-        pathname: '/web/**'
-      }
-    ],
   },
   publicRuntimeConfig: {
     MINIO_ENDPOINT: process.env.MINIO_ENDPOINT
