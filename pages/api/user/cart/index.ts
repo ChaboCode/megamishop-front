bd9d54f84b59393c1ed8a4fd77067efe8452d011
ChaboCode <kaerdos.dev@gmail.com>
2023-10-30 13:55:47 -0600

    Small fix

--- pages/api/user/cart/index.ts
+++ pages/api/user/cart/index.ts
@@ -7,7 +7,7 @@ function GetUserCart(req: NextApiRequest, res: NextApiResponse) {
     const { uid } = req.query
 
     async function query() {
-        const carts = await prisma.cart.findMany({
+        const cartQuery = await prisma.cart.findMany({
             where: {
                 uid: uid as string,
                 isPurchased: false
@@ -17,67 +17,76 @@ function GetUserCart(req: NextApiRequest, res: NextApiResponse) {
             // with the product data. It should be a better way of writing this, but I'm
             // tired asf, and I'm only wondering when this will get functional
             select: {
+                id: true,
                 products: {
-                    select: {
-                        id: true,
-                        product: {
-                            select: {
-                                id: true,
-                                name: true,
-                                price: true,
-                                // TODO: Implement product discounts
-                            }
-                        },
-                        quantity: true
-                    }
                 }
-            },
-            orderBy: {
-                createdAt: 'desc'
             },
             take: 1,
         })
-        if (carts.length > 0) {
-            const total = carts[0].products.reduce((total, cartProduct) => {
-                const { price } = cartProduct.product
-                const { quantity } = cartProduct
-                return total + price.toNumber() * quantity
-            }, 0)
 
-            const cart: ICart = {
-                products: carts[0].products.map(cartProduct => {
-                    const product: ICartProduct = {
-                        cartProductId: cartProduct.id,
-                        productID: cartProduct.product.id,
-                        title: cartProduct.product.name,
-                        price: cartProduct.product.price.toNumber(),
-                        quantity: cartProduct.quantity,
-                    }
-                    return product
-                }),
-                total: total,
-                success: true,
-            }
+        if (cartQuery.length < 1) {
+            return {
+                success: false
+            } as ServerResponse
 
-            return cart
         }
 
-        return {
-            success: false
-        } as ServerResponse
+        const products = await prisma.cart_product.findMany({
+            where: {
+                cartID: cartQuery[0].id,
+            },
+            select: {
+                id: true,
+                product: {
+                    select: {
+                        id: true,
+                        name: true,
+                        price: true,
+                        // TODO: Implement product discounts
+                    }
+                },
+                quantity: true
+            },
+            orderBy: {
+                addedDate: 'desc'
+            }
+        })
+
+        const total = products.reduce((total, cartProduct) => {
+            const { price } = cartProduct.product
+            const { quantity } = cartProduct
+            return total + price.toNumber() * quantity
+        }, 0)
+
+        const cart: ICart = {
+            products: products.map(cartProduct => {
+                const product: ICartProduct = {
+                    cartProductId: cartProduct.id,
+                    productID: cartProduct.product.id,
+                    title: cartProduct.product.name,
+                    price: cartProduct.product.price.toNumber(),
+                    quantity: cartProduct.quantity,
+                }
+                return product
+            }),
+            total: total,
+            success: true,
+        }
+
+        return cart
     }
+
 
     query()
         .then(async cart => {
             await prisma.$disconnect()
             res.status(200).json(cart)
+            res.end()
         })
         .catch(async err => {
             await prisma.$disconnect()
             res.status(500)
             console.error(err)
-        })
-        .finally(() => {
             res.end()
         })
 }
